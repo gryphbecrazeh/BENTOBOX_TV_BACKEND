c1de350d065f81291819ac5fb026da1f7d4c1ac8
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-04-20 18:22:23 -0400

    removed check to see video url already exists / force update

--- routes/api/episode.js
+++ routes/api/episode.js
@@ -10,17 +10,17 @@ router.get("/", (req, res) => {
 	let { episode } = req.query;
 	Video.findById(episode)
 		.then((ep) => {
-			if (!ep.video) {
-				let scraper = new VideoScraper();
-				scraper.getVideo(ep.link).then((link) => {
-					Video.findByIdAndUpdate(episode, { video: link }).catch((err) => {
-						res.status(400).json({ msg: "Unable to update file..." });
-					});
-					res.status(200).json({ ...ep, video: link });
+			// if (!ep.video) {
+			let scraper = new VideoScraper();
+			scraper.getVideo(ep.link).then((link) => {
+				Video.findByIdAndUpdate(episode, { video: link }).catch((err) => {
+					res.status(400).json({ msg: "Unable to update file..." });
 				});
-			} else {
-				res.status(200).json(ep);
-			}
+				res.status(200).json({ ...ep, video: link });
+			});
+			// } else {
+			// res.status(200).json(ep);
+			// }
 		})
 		.catch((err) => console.log(err));
 });
