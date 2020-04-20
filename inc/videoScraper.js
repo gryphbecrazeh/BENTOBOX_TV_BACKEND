38522334c1f32c159579b5161d9ba34b49099102
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-04-20 19:55:37 -0400

    forgot to add flags to video scraper

--- inc/videoScraper.js
+++ inc/videoScraper.js
@@ -4,18 +4,23 @@ class VideoScraper {
 			let videoUrl;
 			const puppeteer = require("puppeteer");
 
-			return puppeteer.launch().then(async (browser) => {
-				const page = await browser.newPage();
-				page.on("response", (response) => {
-					let resUrl = response["_url"];
-					if (resUrl.match(/s.\.mp4\.sh\/\w+\//gim)) {
-						videoUrl = resUrl;
-					}
+			return puppeteer
+				.launch({
+					headless: true,
+					args: ["--no-sandbox", "--disable-setuid-sandbox"],
+				})
+				.then(async (browser) => {
+					const page = await browser.newPage();
+					page.on("response", (response) => {
+						let resUrl = response["_url"];
+						if (resUrl.match(/s.\.mp4\.sh\/\w+\//gim)) {
+							videoUrl = resUrl;
+						}
+					});
+					await page.goto(url);
+					await browser.close();
+					return await videoUrl;
 				});
-				await page.goto(url);
-				await browser.close();
-				return await videoUrl;
-			});
 		};
 	}
 }
