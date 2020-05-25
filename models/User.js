f7c2ff516738badf0dfae7ed6428a153df147d58
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-05-25 12:04:23 -0400

    Modified 'Read Me' to display newly found bugs, goals, and clear fixed ones, added User model to 'models' beginning to create users

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Series Schema
+const UserSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	watchLater: {
+		type: Array,
+	},
+	watchHistory: {
+		type: Array,
+	},
+	favorites: {
+		type: Array,
+	},
+	watchingList: {
+		type: Array,
+	},
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
